c12619d50037640275b57fa292f68f3be04acefa
Tyler <tyler.p.jewell@gmail.com>
2022-03-08 20:52:26 -0700

    adding sensor_plus example

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,13 +1,13 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+// // Copyright (c) 2022, Very Good Ventures
+// // https://verygood.ventures
+// //
+// // Use of this source code is governed by an MIT-style
+// // license that can be found in the LICENSE file or at
+// // https://opensource.org/licenses/MIT.
 
-import 'package:star_oven/app/app.dart';
-import 'package:star_oven/bootstrap.dart';
+// import 'package:star_oven/app/app.dart';
+// import 'package:star_oven/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
-}
+// void main() {
+//   bootstrap(() => const App());
+// }
